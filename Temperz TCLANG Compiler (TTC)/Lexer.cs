3ece4b19f339ae790ccecaf7886d01e50c320476
DESKTOP-SM4PJFO\tempe <temperz871@gmail.com>
2023-12-27 19:48:32 -0500

    The lexer now does things in order, let's go!!!!

--- Temperz TCLANG Compiler (TTC)/Lexer.cs
+++ Temperz TCLANG Compiler (TTC)/Lexer.cs
@@ -11,24 +11,43 @@ public class Lexer
     private static string[] keywords = new string[] { "bool", "break", "calloc", "char", "const", "continue", "else", "elseif", "enum", "false", "for", "float", "free", "goto", "if", "int", "lengthof", "malloc", "nextbyte", "outputbyte", "realloc", "return", "sizeof", "static", "string", "struct", "true", "typeof", "void", "while" }; // I definitley missed one of these
     private static string[] punctuators = new string[] { "{", "}", ";", "(", ")", "[", "]", "," };
 
-    public static IEnumerable<Token>GetTokens(string[] code)
+    private const bool DEBUG = false;
+    private const bool VERBOSE = false;
+
+    public static IEnumerable<Token> GetTokens(string[] code)
     {
+        PriorityQueue<TokenInfo, int> queue = new PriorityQueue<TokenInfo, int>(); // This is the first time i've ever used this cursed data structureww
+
         foreach (string line in code)
         {
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+
+            if (DEBUG)
+                Console.WriteLine("Now handling line: " + line);
             string currToken = line;
 
-            IEnumerable<Token> findTokens(string[] list, TokenType tokenType)
+            IEnumerable<TokenInfo> findTokens(string[] list, TokenType tokenType)
             {
                 foreach (string toFind in list)
                 {
                     while (currToken.IndexOf(toFind) != -1)
                     {
-                        yield return new Token(toFind, tokenType);
+                        yield return new TokenInfo(toFind, tokenType, currToken.IndexOf(toFind));
+
 
                         int startIdx = currToken.IndexOf(toFind);
                         int endIdx = startIdx + toFind.Length;
 
-                        currToken = (startIdx > 0 ? currToken.Substring(0, startIdx) + (" ") : "") + (endIdx < currToken.Length ? currToken.Substring(endIdx + 1) : "");
+                        if (DEBUG && VERBOSE)
+                        {
+                            Console.WriteLine("Found token " + toFind + " at " + currToken.IndexOf(toFind));
+                            Console.WriteLine("Curr token before: " + currToken);
+                        }
+                        currToken = ReplaceWhiteSpace(currToken, startIdx, endIdx);
+                        if (DEBUG && VERBOSE)
+                            Console.WriteLine("Curr token after: " + currToken);
                     }
                 }
             }
@@ -54,17 +73,18 @@ public class Lexer
                 int startIdx = currToken.IndexOf("\"");
                 int endIdx = startIdx + currToken.Substring(startIdx + 1).IndexOf("\"") + 1;
                 string token = currToken.Substring(startIdx, endIdx - startIdx + 1);
-                yield return new Token(token, TokenType.Literal);
+                queue.Enqueue(new TokenInfo(token, TokenType.Literal, currToken.IndexOf(token)), currToken.IndexOf(token));
 
-                currToken = (startIdx > 0 ? currToken.Substring(0, startIdx) + (" ") : "") + (endIdx < currToken.Length ? currToken.Substring(endIdx + 1) : "");
+                //currToken = (startIdx > 0 ? currToken.Substring(0, startIdx) + (" ") : "") + (endIdx < currToken.Length ? currToken.Substring(endIdx) : "");
+                currToken = ReplaceWhiteSpace(currToken, startIdx, endIdx + 1);
             }
 
-            foreach (Token t in findTokens(operators, TokenType.Operator))
-                yield return t;
-            foreach (Token t in findTokens(keywords, TokenType.Keyword))
-                yield return t;
-            foreach (Token t in findTokens(punctuators, TokenType.Punctuator))
-                yield return t;
+            foreach (TokenInfo t in findTokens(operators, TokenType.Operator))
+                queue.Enqueue(t, t.idx);
+            foreach (TokenInfo t in findTokens(keywords, TokenType.Keyword))
+                queue.Enqueue(t, t.idx);
+            foreach (TokenInfo t in findTokens(punctuators, TokenType.Punctuator))
+                queue.Enqueue(t, t.idx);
 
             foreach (string identifier in Regex.Split(currToken, @"\s"))
             {
@@ -73,15 +93,70 @@ public class Lexer
 
                 char startChar = identifier[0];
                 if ((char)(startChar & ~32) >= 'A' && (char)(startChar & ~32) <= 'Z' || startChar == '_')
-                    yield return new Token(identifier, TokenType.Identifier);
+                    queue.Enqueue(new TokenInfo(identifier, TokenType.Identifier, currToken.IndexOf(identifier)), currToken.IndexOf(identifier));
                 else if (int.TryParse(identifier, out _))
-                    yield return new Token(identifier, TokenType.Constant);
+                    queue.Enqueue(new TokenInfo(identifier, TokenType.Constant, currToken.IndexOf(identifier)), currToken.IndexOf(identifier));
                 else
                     throw new Exception("Failed to find valid token type for " + identifier);
+
+                currToken = ReplaceWhiteSpace(currToken, currToken.IndexOf(identifier), currToken.IndexOf(identifier) + identifier.Length);
             }
+
+
+            while (queue.Count > 0)
+            {
+                TokenInfo t = queue.Dequeue();
+                //Console.WriteLine("Deuqueued " + t.token.Name + " at " + t.idx);
+                yield return t.token;
+            }
+
+            if (DEBUG)
+                Console.WriteLine();
+        }
+    }
+
+    private static string ReplaceWhiteSpace(string toWhiteSpace, int startIdx, int endIdx)
+    {
+        if (startIdx < -1)
+            throw new Exception("start idx was negative!");
+        else if (toWhiteSpace.Length < endIdx)
+            throw new Exception("end idx was out of bounds!");
+        else if (endIdx < startIdx)
+            throw new Exception("end idx was less than start idx!");
+
+        string result = "";
+        if (startIdx > 0)
+            result = toWhiteSpace.Substring(0, startIdx);
+        for (int i = startIdx; i < endIdx; i++)
+            result += " ";
+
+
+        if (endIdx < toWhiteSpace.Length)
+            result += toWhiteSpace.Substring(endIdx);
+        return result;
+    }
+
+    public struct TokenInfo
+    {
+        public Token token;
+        public int idx;
+
+        public TokenInfo(string name, TokenType type, int idx)
+        {
+            this.token = new Token(name, type);
+            this.idx = idx;
+        }
+
+        public TokenInfo(Token token, int idx)
+        {
+            this.token = token;
+            this.idx = idx;
         }
     }
 }
+
+
+// TODO: Move this into its own .cs file
 
 public enum TokenType
 {
